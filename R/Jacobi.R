903ed6e7f5416d5db6927af00bb4fd7a3e1c4ce3
cserranocolome <47814831+cserranocolome@users.noreply.github.com>
2019-02-27 08:22:48 +0100

    Update Jacobi.R
    Exchange Jacobi functions from master and Jacobi branch (in order to use the function in Jacobi branch in the master)

--- R/Jacobi.R
+++ R/Jacobi.R
@@ -14,20 +14,22 @@
 
 Jacobi <- function(x, tol=.Machine$double.eps){
   n <- nrow(x)
-  
+  V <- diag(n)
   while(offA(x)>tol){
     for(k in 1:(n-1)){
       f <- floor((n-k)/2)
       if(f!=(n-k)/2){
         f <- f+1
-        }
+      }
       
       is <- seq(1,f,1)
       js <- n-k+2-is
       
       for(i in 1:length(is)){
         if(x[is[i],js[i]]!=0){
-          x <- changes(x,is[i],js[i])
+          sct <- fsct(x,is[i],js[i])
+          x <- changes(x,is[i],js[i],sct)
+          V <- buildV(V,is[i],js[i],sct) 
         }
       }
       
@@ -36,11 +38,13 @@ Jacobi <- function(x, tol=.Machine$double.eps){
         js2 <- 2*n-k+2-is2
         for(i in 1:length(is2)){
           if(x[is2[i],js2[i]]!=0){
-           x <- changes(x,is2[i],js2[i])
-          }            
+            sct <- fsct(x,is2[i],js2[i])
+            x <- changes(x,is2[i],js2[i],sct)
+            V <- buildV(V,is2[i],js2[i],sct)          
+          }
         }
       }
-      }
+    }
     
     f <- floor(n/2)
     
@@ -53,11 +57,13 @@ Jacobi <- function(x, tol=.Machine$double.eps){
     
     for(i in 1:length(is3)){
       if(x[is3[i],js3[i]]!=0){
-        x <- changes(x,is3[i],js3[i])
-      }            
+        sct <- fsct(x,is3[i],js3[i])
+        x <- changes(x,is3[i],js3[i],sct)
+        V <- buildV(V,is3[i],js3[i],sct)           
+      }
     }
   }
-  ans <- list(values=diag(x))
+  ans <- list(values=diag(x), vectors = V)
   ans
 }
 
