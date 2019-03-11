0ccb8fce39b749fe85f0a7f2383c2b0977bf1c52
Bioinformatic Research Group in Epidemiology (BRGE) <juanr.gonzalez@isglobal.org>
2019-03-11 09:54:25 +0100

    uploaded paralellSVD

---
+++ R/parallelSVD.R
@@ -0,0 +1,38 @@
+parallelSVD <- function(x, ncomponents, k, ...){
+  
+  xx <- 
+    
+  svdPartial <- function(x){
+    ss <- svd(x)
+    ans <- sweep(ss$u, 2, FUN="*", ss$d)
+    return(ans)
+  }  
+  
+  ll <- lapply(x, svd)
+  X <- Reduce(ll, cbind)
+  ans <- svd(X)
+  ans
+  
+}
+
+
+
+
+x1  <- matrix(rnorm(10000), nrow=1000)
+x2  <- matrix(rnorm(10000), nrow=1000)
+
+s1 <- svd(t(x1))
+s2 <- svd(t(x2))
+
+
+m1 <- sweep(s1$u, 2, FUN="*", s1$d)
+m2 <- sweep(s2$u, 2, FUN="*", s2$d)
+m <- cbind(m1,m2)
+
+ss1 <- svd(m)
+
+ss2 <- svd(cbind(t(x1), t(x2)))
+
+X <- cbind(t(x1), t(x2))
+
+ss3 <- blockSVD(X, q=1, k=nrow(x1))
